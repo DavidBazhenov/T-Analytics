95db9c27c1ddcd24e8154a65065d9de7cbf5200d
DavidBazhenov <dav.id.bazhe.nov7@gmail.com>
2024-11-22 22:58:31 +0300

    0.0.0.0 is added

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ dotenv.config(); // Загружаем переменные окружения
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   const config = new DocumentBuilder()
     .setTitle('Expense Tracker API')
     .setDescription('API for managing expenses and transactions')
@@ -16,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
